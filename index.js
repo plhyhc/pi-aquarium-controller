48884af5edbf088dfe5162d8d77bdb76aea5f4df
Josh Kraft <josh.h.kraft@gmail.com>
2022-01-04 17:22:38 -0800

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+var sensor = require('node-dht-sensor');
+var ds18b20 = require('ds18b20');
+
+ds18b20.sensors((err, ids) => {
+  console.log([err, ids]);
+});
+// 28-3c01f096bcde
+
+ds18b20.temperature('28-3c01f096bcde', (err, value) => {
+  const temp = (value * (9/5)) + 32;
+  console.log('Current temperature is', temp);
+});
+
+sensor.read(22, 5, (err, temp, humid) => {
+	if(!err) {
+	console.log(`temp: ${temp}, humidity: ${humid}`);
+	} else {
+	console.log(err);
+	}
+});
